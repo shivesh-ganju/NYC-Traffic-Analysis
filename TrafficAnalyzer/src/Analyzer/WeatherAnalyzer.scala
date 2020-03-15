61188456f123ec920540f1ff0b46d64894a402cd
shivesh-ganju <shiveshganju@gmail.com>
2020-03-15 17:46:02 -0400

    Sample test

--- TrafficAnalyzer/src/Analyzer/WeatherAnalyzer.scala
+++ TrafficAnalyzer/src/Analyzer/WeatherAnalyzer.scala
@@ -1,5 +1,8 @@
 package Analyzer
 
 class WeatherAnalyzer {
-  
+  def sampleTest(){
+    val data = sc.textFile("loudacre/weblog/*")
+    data.take(2)
+  }
 }
--- TrafficAnalyzer/src/Visualization/TableauIntegrator.scala
+++ TrafficAnalyzer/src/Visualization/TableauIntegrator.scala
@@ -1,5 +1,8 @@
 package Visualization
-
-class TableauIntegrator {
-  
+import Analyzer.WeatherAnalyzer;
+object TableauIntegrator {
+  def main(args: Array[String]){
+    var a = new WeatherAnalyzer()
+    a.sampleTest()
+  }
 }
